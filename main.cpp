82acc319da8f4fb30385302f5a7744e32b38f8d2
Matheus <msousa@sjtu.edu.cn>
2025-05-18 17:13:46 +0800

    refactor common used

--- main.cpp
+++ main.cpp
@@ -1,40 +1,25 @@
-#include <cstdint>
-#include <cstring>
-#include <iostream>
-#include <stdexcept>
 #include <sys/mman.h>
 #include <unistd.h>
 
+#include <cstdint>
+#include <cstring>
+#include <initializer_list>
+#include <iostream>
+#include <stdexcept>
+
 #include "simple_tdd.h"
 
+typedef int32_t i32;
+typedef uint32_t u32;
+typedef uint64_t u64;
 typedef int64_t i64;
+typedef uint16_t u16;
+typedef int16_t i16;
 typedef uint8_t u8;
-
-struct x86_64_Instr {
-  static const u8 REXW = 0x48;      // REX prefix for 64-bit operand size
-  static const u8 MOV_IMM = 0xB8;   // Move immediate to register
-  static const u8 MOV_REG = 0x89;   // Move register to register
-  static const u8 MOV_MEM_X = 0x8B; // Move memory to register/memory
-  static const u8 MOV_IMM_X = 0xC7; // Move immediate to register/memory
-
-  static const u8 ADD_IMM_X = 0x83; // Add sign-extended immediate to register
-  static const u8 ADD_REG = 0x01;   // Add register to register
-  static const u8 ADD_MEM_X = 0x03; // Add register to memory
-
-  static const u8 SUB_IMM_X = 0x83; // Subtract immediate from register/memory
-
-  static const u8 PUSH_RBP = 0x55; // Push RBP onto the stack
-  static const u8 POP_RBP = 0x5D;  // Pop RBP from the stack
-
-  static const u8 RET = 0xC3; // Return from function
-};
+typedef int8_t i8;
 
 class Jit {
-private:
-  void *data_;
-  size_t size_;
-
-public:
+ public:
   Jit(size_t size) : size_(size) {
     // Allocate memory with read, write, and execute permissions
     data_ = mmap(nullptr, size, PROT_READ | PROT_WRITE | PROT_EXEC,
@@ -60,6 +45,91 @@ public:
       throw std::runtime_error("Failed to change memory protection");
     }
   }
+
+ private:
+  void *data_;
+  size_t size_;
+};
+
+#define expand_imm32(x)                                                       \
+  static_cast<u8>(x >> 0), static_cast<u8>(x >> 8), static_cast<u8>(x >> 16), \
+      static_cast<u8>(x >> 24)
+
+struct x86_64_Instr {
+  static const u8 REXW = 0x48;       // REX prefix for 64-bit operand size
+  static const u8 MOV_REG = 0x89;    // Move register to register
+  static const u8 MOV_MEM_X = 0x8B;  // Move memory to register/memory
+  static const u8 MOV_IMM_X = 0xC7;  // Move immediate to register/memory
+
+  static const u8 ADD_IMM_X = 0x83;  // Add sign-extended immediate to register
+  static const u8 ADD_MEM_X = 0x03;  // Add register to memory
+
+  static const u8 SUB_IMM_X = 0x83;  // Subtract immediate from register/memory
+
+  static const u8 PUSH_RBP = 0x55;  // Push RBP onto the stack
+  static const u8 POP_RBP = 0x5D;   // Pop RBP from the stack
+
+  static const u8 RET = 0xC3;  // Return from function
+
+  // Functions
+  static std::vector<u8> sub_rsp_imm_8(u8 imm) {
+    return {REXW, SUB_IMM_X, 0xEC, imm};
+  }
+
+  static std::vector<u8> add_rsp_imm_8(u8 imm) {
+    return {REXW, ADD_IMM_X, 0xC4, imm};
+  }
+
+  static std::vector<u8> mov_to_stack_offset_imm32(u8 offset, u32 imm) {
+    return {REXW, MOV_IMM_X, 0x04, offset, expand_imm32(imm)};
+  }
+};
+
+class AsmStream {
+ public:
+  AsmStream() = default;
+
+  void append(const std::vector<u8> &code) {
+    code_.insert(code_.end(), code.begin(), code.end());
+  }
+
+  void append(std::initializer_list<u8> lst) { code_.insert(code_.end(), lst); }
+
+  u8 *data() { return code_.data(); }
+
+  size_t size() { return code_.size(); }
+
+  void function_prologue() {
+    append({
+        x86_64_Instr::PUSH_RBP,  //
+        x86_64_Instr::REXW,      //
+        x86_64_Instr::MOV_REG,   //
+        0xE5,                    // mov rbp,rsp
+    });
+  }
+
+  void reserve_stack(size_t count) {
+    append(x86_64_Instr::sub_rsp_imm_8(static_cast<u8>(count)));
+  }
+
+  void restore_stack(size_t count) {
+    append(x86_64_Instr::add_rsp_imm_8(static_cast<u8>(count)));
+  }
+
+  void function_epilogue() {
+    append({
+        x86_64_Instr::POP_RBP,
+    });
+  }
+
+  void function_return() {
+    append({
+        x86_64_Instr::RET,
+    });
+  }
+
+ private:
+  std::vector<u8> code_;
 };
 
 typedef i64 (*JitConstant)();
@@ -71,14 +141,11 @@ int make_constant(int value) {
   Jit jit(4096);
 
   u8 code[] = {
-      x86_64_Instr::REXW,           //
-      x86_64_Instr::MOV_IMM_X,      //
-      0xC0,                         //
-      static_cast<u8>(value >> 0),  //
-      static_cast<u8>(value >> 8),  //
-      static_cast<u8>(value >> 16), //
-      static_cast<u8>(value >> 24), //
-      x86_64_Instr::RET,            // ret
+      x86_64_Instr::REXW,       //
+      x86_64_Instr::MOV_IMM_X,  //
+      0xC0,                     //
+      expand_imm32(value),      //
+      x86_64_Instr::RET,        // ret
   };
 
   // Copy the code to the JIT memory
@@ -102,57 +169,30 @@ i64 make_identity(i64 value) {
   // }
   Jit jit(4096);
 
-  std::vector<u8> code;
+  AsmStream code;
 
-  code.insert(code.end(), {
-    // Prologue
-    x86_64_Instr::PUSH_RBP, //
-    x86_64_Instr::REXW,     //
-    x86_64_Instr::MOV_REG,  //
-    0xE5,                   // mov rbp,rsp
+  code.function_prologue();
+  code.reserve_stack(16);
+
+  code.append({
+      // Store the parameter (RDI) at [rsp]
+      x86_64_Instr::REXW,     //
+      x86_64_Instr::MOV_REG,  //
+      0x3C,                   //
+      0x24,                   // mov    QWORD PTR [rsp],rdi
   });
 
-  code.insert(code.end(), {
-    // Reserve stack space
-    x86_64_Instr::REXW,      //
-    x86_64_Instr::SUB_IMM_X, //
-    0xEC,                    //
-    0x10,                    // sub rsp, 16
+  code.append({
+      // Move parameter (RDI) to RAX
+      x86_64_Instr::REXW,       //
+      x86_64_Instr::MOV_MEM_X,  //
+      0x04,                     //
+      0x24,                     // mov    rax,QWORD PTR [rsp]
   });
 
-  code.insert(code.end(), {
-    // Store the parameter (RDI) at [rsp]
-    x86_64_Instr::REXW,    //
-    x86_64_Instr::MOV_REG, //
-    0x3C,                  //
-    0x24,                  // mov    QWORD PTR [rsp],rdi
-  });
-
-  code.insert(code.end(), {
-    // Move parameter (RDI) to RAX
-    x86_64_Instr::REXW,      //
-    x86_64_Instr::MOV_MEM_X, //
-    0x04,                    //
-    0x24,                    // mov    rax,QWORD PTR [rsp]
-  });
-
-  code.insert(code.end(), {
-    // Restore the stack pointer
-    x86_64_Instr::REXW,      //
-    x86_64_Instr::ADD_IMM_X, //
-    0xC4,                    //
-    0x10,                    // Value to add (16)
-  });
-
-  code.insert(code.end(), {
-    // Epilogue
-    x86_64_Instr::POP_RBP,
-  });
-
-  code.insert(code.end(), {
-    // Return
-    x86_64_Instr::RET,
-  });;
+  code.restore_stack(16);
+  code.function_epilogue();
+  code.function_return();
 
   // Copy the code to the JIT memory
   std::memcpy(jit.data(), code.data(), code.size());
@@ -176,68 +216,30 @@ i64 make_increment(i64 value) {
   // }
   Jit jit(4096);
 
-  std::vector<u8> code;
+  AsmStream code;
+  code.function_prologue();
+  code.reserve_stack(16);
 
-  code.insert(code.end(), {
-      // Prologue
-      x86_64_Instr::PUSH_RBP, //
+  code.append(x86_64_Instr::mov_to_stack_offset_imm32(0x24, 1));
+
+  code.append({
+      // Move parameter (RDI) to RAX
       x86_64_Instr::REXW,     //
       x86_64_Instr::MOV_REG,  //
-      0xE5,                   // mov rbp,rsp
+      0xF8,                   // mov rax,rdi
   });
 
-  code.insert(code.end(), {
-      // Reserve stack space
-      x86_64_Instr::REXW,      //
-      x86_64_Instr::SUB_IMM_X, //
-      0xEC,                    //
-      0x10,                    // sub rsp, 16
-  });
-
-  code.insert(code.end(), {
-      // Store the increment value (1) at [rsp]
-      x86_64_Instr::REXW,      //
-      x86_64_Instr::MOV_IMM_X, //
-      0x04,                    //
-      0x24,                    //
-      0x1,                     // Value to store (1)
-      0x0,                     //
-      0x0,                     //
-      0x0,                     // mov QWORD PTR [rsp], 1
-  });
-  
-  code.insert(code.end(), {
-      // Move parameter (RDI) to RAX
-      x86_64_Instr::REXW,    //
-      x86_64_Instr::MOV_REG, //
-      0xF8,                  // mov rax,rdi
-  });
-
-  code.insert(code.end(), {
+  code.append({
       // Add the increment value to RAX
-      x86_64_Instr::REXW,      //
-      x86_64_Instr::ADD_MEM_X, //
-      0x04,                    //
-      0x24,                    // add rax,QWORD PTR [rsp]
+      x86_64_Instr::REXW,       //
+      x86_64_Instr::ADD_MEM_X,  //
+      0x04,                     //
+      0x24,                     // add rax,QWORD PTR [rsp]
   });
 
-  code.insert(code.end(), {
-      // Restore the stack pointer
-      x86_64_Instr::REXW,      //
-      x86_64_Instr::ADD_IMM_X, //
-      0xC4,                    //
-      0x10,                    // Value to add (16)
-  });
-
-  code.insert(code.end(), {
-      // Epilogue
-      x86_64_Instr::POP_RBP,
-  });
-
-  code.insert(code.end(), {
-      // Return
-      x86_64_Instr::RET,
-  });
+  code.restore_stack(16);
+  code.function_epilogue();
+  code.function_return();
 
   // Copy the code to the JIT memory
   std::memcpy(jit.data(), code.data(), code.size());
